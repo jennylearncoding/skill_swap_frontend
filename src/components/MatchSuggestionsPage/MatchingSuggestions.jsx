10feb1d0c070d17cb0e8b7d8784ef04ab239b551
jenny xu <jingwenxu13@gmail.com>
2025-08-06 14:49:39 -0700

    delete dropdown

--- src/components/MatchSuggestionsPage/MatchingSuggestions.jsx
+++ src/components/MatchSuggestionsPage/MatchingSuggestions.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./MatchingSuggestions.css";
 import MatchCard from "../MatchCard/MatchCard";
-import UserDropdown from "../UserDropdown/UserDropdown";
 import ProfilePage from "../ProfilePage/ProfilePage";
 import { API_URL } from "../../App";
 
